df86df3a9f5531d2933cdf7a980c65c5d1169112
Chris Phelps <c.j.phelps89@gmail.com>
2024-07-27 01:42:07 -0400

    Updated code to add async calls for data from API

--- index.js
+++ index.js
@@ -1,15 +1,106 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const fetch = require("node-fetch");
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, input, licenseKey) {
+  // call the generate markdown function passed from from the utils folder
+  markdown(input, licenseKey)
+    .then((strTextContent) => {
+      // the markdown function returns a readme based on the data passed to the function from the user input and fetch requests
+      // we then write the data returned to a file or display an error if there is an issue in doing so
+      fs.writeFile(fileName, strTextContent, (err) => {
+        if (err) {
+          console.error("Error writing file:", err);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error generating markdown content:", error);
+    });
+}
+
+function createArray(arrLicense){
+  // take our array of license objects and map them to a new array and only save the value used for Name
+  // array went from looking like [{Name:val1, Key:val1},{Name:val2, Key:val2}] to [Name1, Name2]
+  const arrLicenseNames = arrLicense.map(({ Name }) => Name);
+  // TODO: Create an array of questions for user input
+  // this array relies on data passed by 
+  const arrQuestions = [
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "inTitle",
+    },
+    {
+      type: "input",
+      message: "Enter a description for your project:",
+      name: "inDecription",
+    },
+    { type: "input", message: "Please enter steps required for installation:", name: "inInstall" },
+    {
+      type: "input",
+      message: "Please enter instructions for application usage:",
+      name: "inUsage",
+    },
+    {
+      type: "list",
+      message: "Please select the license you will be using from the following:",
+      name: "inLicense",
+      choices: arrLicenseNames, //Use our array of licenses
+    },
+    { type: "input", message: "Please note any contribution guidelines to the project below:", name: "inContribute" },
+    { type: "input", message: "Please not any instructions for testing below:", name: "inTesting" },
+    { type: "input", message: "Please enter your github username:", name: "inUser" },
+    { type: "input", message: "Please enter your email address:", name: "inEmail" },
+  ];
+  // pass our array of questions and our array of licenses to the runinquirer function
+  runInquirer(arrLicense, arrQuestions) 
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function runInquirer(arrLicense, arrQuestions) {
+  
+  inquirer //Run inquirer prompt for each question in the array of questions
+    .prompt(arrQuestions)
+    .then((response) => {
+      const licenseKey = arrLicense.find((val) => val.Name === response.inLicense);
+      // create our filename based off the project name
+      let fileName = `${response.inTitle.replaceAll(" ", "-")}-README.md`;
+      writeToFile(fileName, response, licenseKey);
+    });
+}
+
+// I wanted a way to retrieve the license lists from githubs API, so I did a bit of research and with the help of xpert
+// was able to figure out how to get fetch requests workign using node-fetch, note the syntax is the same as an HTML fetch
+// but requires the added node-fetch object initialized above using version 2 (npm i node-fetch@2):
+function init() {
+  // initialize the array to store license info
+  let arrLicenses = [];
+  fetch("https://api.github.com/licenses") //fetch the license info from github API
+    .then((response) => {
+      if (!response.ok) {
+        // response no good display error
+        console.error(`Error:${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // data is good lets get the values we want and store in an array of objects
+      for (let arr of data) {
+        arrLicenses.push({ Name: arr.name, Key: arr.key });
+      }
+      // push a final value that allows users to select none as their license
+      arrLicenses.push({ Name: "None", Key: "" });
+      // run our next function to create questions used to prompt the user
+      createArray(arrLicenses);
+    })
+    .catch((error) => {
+      console.error(`Unable to connect to github APIs. Error: ${error}`);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(short) {
+  const imgBadge = `https://img.shields.io/badge/License:-${short.replaceAll(" ", "_").replaceAll("-", "_")}-red`;
+  return imgBadge;
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// If there is no license => IF THERE IS NO LICENSE this function will never be called
+function renderLicenseLink(license) {
+  return fetch(`https://api.github.com/licenses/${license.Key}`)
+    .then((response) => {
+      if (!response.ok) {
+        console.error(`Error:${response.statusText}`);
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return { url: data.html_url, short: data.spdx_id };
+    })
+    .catch((error) => {
+      console.error(`Unable to connect to github APIs. Error: ${error}`);
+      throw error;
+    });
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// If there is no license => IF THERE IS NO LICENSE this function will never be called
+function renderLicenseSection(license) {
+  return renderLicenseLink(license)
+    .then((value) => {
+      const imgValue = renderLicenseBadge(value.short);
+      return `[![License:](${imgValue})](${value.url})`;
+    })
+    .catch((err) => {
+      console.error(`Error: ${err}`);
+      throw err;
+    });
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(input, licenseKey) {
+  // if there is a license, redirect to renderlicense functions
+  if (licenseKey.Key) {
+    return renderLicenseSection(licenseKey)
+      .then((data) => {
+        return generateMarkdownBody(input, data);
+      })
+      .catch((err) => {
+        console.error(`Error: ${err}`);
+        return "";
+      });
+  }
+  // there is no license so we handle the code execution here
+  else {
+    return generateMarkdownBody(input);
+  }
 }
+
+// create and return a template string with the data passed via input parameter
+function generateMarkdownBody(input, licenseData) {
+  console.log("--------------------");
+  //inTitle inDecription inInstall inUsage licenseData (license) inContribute inTesting inUser inEmail
+  console.log(input.inTitle); 
+  let strMD = `#${inTitle}`
+  console.log(licenseData); 
+  return "";
+}
+
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation,
+// Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 
 module.exports = generateMarkdown;
